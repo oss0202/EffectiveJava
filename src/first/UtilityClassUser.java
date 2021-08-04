77799dc3ee487b81f487bcd2ed824f65ca338fb5
오성식 <oss0202@naver.com>
2021-08-04 15:33:14 +0900

    4. 인스턴스화를 막으려거든 private 생성자를 사용하라

---
+++ src/first/UtilityClassUser.java
@@ -0,0 +1,10 @@
+package first;
+
+public class UtilityClassUser {
+    public static void main(String[] args) {
+        // 'UtilityClass()' has private access in 'first.UtilityClass'
+        // 인스턴스화를 막았기 때문에(private) 불가능하다.
+        // UtilityClass utilityClass = new UtilityClass();
+
+    }
+}
