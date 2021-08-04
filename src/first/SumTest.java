b57e59a04e2721733f07bab9c8275d43ae8e4b32
오성식 <oss0202@naver.com>
2021-08-04 17:06:35 +0900

    6. 불필요한 객체 생성을 피하라

---
+++ src/first/SumTest.java
@@ -0,0 +1,32 @@
+package first;
+
+import javax.xml.transform.Source;
+
+public class SumTest {
+    private static long refererSum(){
+        Long sum = 0L;
+        for (long i = 0; i <= Integer.MAX_VALUE; i++){
+            sum += i;
+        }
+        return sum;// long -> Long 오토박싱
+    }
+    private static long primiSum(){
+        long sum = 0L;
+        for (long i = 0; i <= Integer.MAX_VALUE; i++){
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        long beforeTime = System.currentTimeMillis();
+        System.out.println(SumTest.refererSum());
+        long afterTime = System.currentTimeMillis();
+        System.out.println("refererSum 시간(ms) : " + (afterTime-beforeTime)/60);
+
+        beforeTime = System.currentTimeMillis();
+        System.out.println(SumTest.primiSum());
+        afterTime = System.currentTimeMillis();
+        System.out.println("refererSum 시간(ms) : " + (afterTime-beforeTime)/60);
+    }
+}
