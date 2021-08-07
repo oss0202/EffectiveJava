fecda0fd9a83c90fcf3785523f96ed69e11d6833
오성식 <oss0202@naver.com>
2021-08-07 21:14:18 +0900

    제네릭스 테스트

---
+++ src/etc/UseBox.java
@@ -0,0 +1,9 @@
+package etc;
+
+public class UseBox {
+    public static void main(String[] args) {
+        Box<String> box = new Box<>();
+        box.setItem("123");
+//        box.setItem(new Object());
+    }
+}
