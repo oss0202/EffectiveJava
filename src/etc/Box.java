48704bba8c16a07a9b8b2972b10fde926dbc1cc3
오성식 <oss0202@naver.com>
2021-08-07 21:49:39 +0900

    제네릭스 테스트

--- src/etc/Box.java
+++ src/etc/Box.java
@@ -1,10 +1,17 @@
 package etc;
 
-public class Box<T> {
-    T item;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Box<T, E> {
+    T item;
+    List<E> list = new ArrayList<>();
     public void setItem(T item) {
         this.item = item;
+    }
+
+    public void setList(List<E> list) {
+        this.list = list;
     }
 
     public T getItem() {
--- src/etc/UseBox.java
+++ src/etc/UseBox.java
@@ -1,9 +1,14 @@
 package etc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class UseBox {
     public static void main(String[] args) {
-        Box<String> box = new Box<>();
+        Box<String, Integer> box = new Box<>();
         box.setItem("123");
+        box.setList(Arrays.asList(1,2,3,4,5));
 //        box.setItem(new Object());
+
     }
 }
