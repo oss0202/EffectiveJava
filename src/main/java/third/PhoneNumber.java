6179c5f1d93aec2c5a8aad7f01b11018ba1b1a10
Oh <oss0202@naver.com>
2021-09-26 16:45:38 +0900

    Phone Clone

--- src/main/java/third/PhoneNumber.java
+++ src/main/java/third/PhoneNumber.java
@@ -1,5 +1,7 @@
 package third;
 
+import com.sun.xml.internal.ws.policy.spi.AssertionCreationException;
+
 public class PhoneNumber {
     int areaCode;
     int prefix;
@@ -14,6 +16,15 @@ public class PhoneNumber {
     @Override
     public String toString() {
         return String.format("%03d-%03d-%04d",areaCode, prefix, lineNum);
+    }
+
+    @Override
+    protected PhoneNumber clone() {
+        try {
+            return (PhoneNumber) super.clone();
+        } catch (CloneNotSupportedException e){
+            throw new AssertionError();// 일어날 수 없는 일이다.
+        }
     }
 
     public static void main(String[] args) {
